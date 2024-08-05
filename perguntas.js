730aece3f8da4283491536a73b73947d0562fb45
Patozn <160489571+Patozn@users.noreply.github.com>
2024-08-05 09:41:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,171 @@
+export const perguntas = [
+    {
+        enunciado: " Maria e João estão discutindo sobre a importância das abelhas para o meio ambiente. Maria afirma que as abelhas são fundamentais para a polinização de muitas culturas agrícolas, garantindo a produção de alimentos. João discorda, dizendo que as abelhas não têm impacto significativo na agricultura.",
+        alternativas: [
+            {
+                texto: "Isso é muito bom",
+                afirmacao: [
+                    "Maria está certa. As abelhas desempenham um papel crucial na polinização de diversas plantas cultivadas, contribuindo diretamente para a produção de alimentos.",
+                    "João está errado. As abelhas são essenciais para a agricultura, ajudando na polinização de muitas culturas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Ajudar o meio ambiente",
+                    "lutar contra a contaminação do mundo"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Ana e Pedro estão debatendo sobre a origem do lixo plástico nos oceanos. Ana argumenta que a principal fonte de poluição plástica nos oceanos são os resíduos descartados incorretamente por atividades humanas, como o descarte inadequado de plásticos. Pedro discorda, sugerindo que a maior parte do plástico nos oceanos vem de fontes naturais.?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: [
+                    "Ana está certa. A maior parte do lixo plástico nos oceanos vem de atividades humanas, como descarte inadequado de plásticos e microplásticos..",
+                    "Pedro está enganado. A poluição plástica nos oceanos é predominantemente causada por ações humanas, não por fontes naturais.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Carlos e Sofia discutem sobre o impacto das emissões de gases de efeito estufa no aquecimento global. Carlos argumenta que o aumento das emissões de gases como CO2 e metano contribui diretamente para o aquecimento do planeta. Sofia discorda, afirmando que as mudanças climáticas são causadas principalmente por variações naturais no clima.",
+                afirmacao: [
+                    "Carlos está correto. O aumento das emissões de gases de efeito estufa está diretamente relacionado ao aquecimento global observado nas últimas décadas.",
+                    "Sofia está equivocada. As atividades humanas, especialmente a queima de combustíveis fósseis,",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Lucas e Mariana discutem sobre os benefícios das energias renováveis. Lucas argumenta que as energias renováveis, como solar e eólica, são formas limpas de geração de energia que reduzem as emissões de gases poluentes. Mariana discorda, sugerindo que as energias renováveis são menos confiáveis e mais caras do que as fontes tradicionais de energia.?",
+        alternativas: [
+            {
+                texto: "Lucas está certo. As energias renováveis são formas limpas e sustentáveis de geração de energia",
+                afirmacao: [
+                    "Mariana está enganada. As energias renováveis têm se tornado cada vez mais competitivas em termos de custo ",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Joana e André debatem sobre a importância da conservação das florestas tropicais. Joana argumenta que as florestas tropicais são vitais para a biodiversidade global, abrigando milhões de espécies de plantas e animais. André discorda, sugerindo que as florestas tropicais não têm influência significativa na biodiversidade global.",
+                afirmacao: [
+                    "Joana está correta. As florestas tropicais são consideradas os ecossistemas mais biodiversos do planeta",
+                    "André está equivocado. As florestas tropicais são fundamentais para a biodiversidade global",
+                    
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Daniel e Paula discutem sobre os efeitos da acidificação dos oceanos. Daniel argumenta que a acidificação dos oceanos está aumentando devido à absorção de CO2 atmosférico, o que ameaça os ecossistemas marinhos como recifes de coral. Paula discorda, afirmando que a acidificação dos oceanos não representa uma ameaça significativa para a vida marinha.",
+        alternativas: [
+            {
+            
+                afirmacao: [
+                    "Daniel está certo. A acidificação dos oceanos é uma preocupação crescente devido à absorção de CO2",
+                    "Paula está errada. A acidificação dos oceanos representa uma ameaça real para os ecossistemas marinhos"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
