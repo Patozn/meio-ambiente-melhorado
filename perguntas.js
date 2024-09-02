9253e3834290e78634a626ef3ca5a83238636dda
Patozn <160489571+Patozn@users.noreply.github.com>
2024-09-02 08:31:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,113 +1,111 @@
 export const perguntas = [
     {
-        enunciado: " Maria e João estão discutindo sobre a importância das abelhas para o meio ambiente. Maria afirma que as abelhas são fundamentais para a polinização de muitas culturas agrícolas, garantindo a produção de alimentos. João discorda, dizendo que as abelhas não têm impacto significativo na agricultura.",
+        enunciado: "Clara, uma jovem ambientalista, está conversando com seu amigo Pedro sobre a importância das abelhas para o meio ambiente. Clara acredita que as abelhas são essenciais para a polinização e a produção de alimentos. Pedro acha que as abelhas não têm um impacto significativo na agricultura.",
         alternativas: [
             {
-                texto: "Isso é muito bom",
+                texto: "Você concorda com Clara? Explique por quê.",
                 afirmacao: [
-                    "Maria está certa. As abelhas desempenham um papel crucial na polinização de diversas plantas cultivadas, contribuindo diretamente para a produção de alimentos.",
-                    "João está errado. As abelhas são essenciais para a agricultura, ajudando na polinização de muitas culturas."
+                    "Sim, eu concordo com Clara. As abelhas são cruciais para a polinização de muitas plantas cultivadas, o que é vital para a produção de alimentos.",
+                    "Pedro está errado. Sem as abelhas, muitas culturas agrícolas não teriam uma boa produção."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Você concorda com Pedro? Explique por quê.",
                 afirmacao: [
-                    "Ajudar o meio ambiente",
-                    "lutar contra a contaminação do mundo"
+                    "Não, Pedro está equivocado. As abelhas desempenham um papel fundamental na agricultura e na produção de alimentos.",
+                    "Clara está correta ao destacar a importância das abelhas para o meio ambiente."
+                ],
+                proxima: 1,
+            },
+        ]
+    },
+    {
+        enunciado: "Clara visita uma praia e observa que há muito lixo plástico na água. Ela se lembra da discussão que teve com Pedro e decide investigar a origem do lixo. Clara descobre que muitos resíduos plásticos vêm do descarte inadequado por atividades humanas. Seu amigo Lucas acha que a poluição plástica nos oceanos tem uma origem natural.",
+        alternativas: [
+            {
+                texto: "Você concorda com Lucas? Explique por quê.",
+                afirmacao: [
+                    "Não, Lucas está incorreto. A maior parte da poluição plástica nos oceanos vem do descarte inadequado de plásticos e não de fontes naturais.",
+                    "A atividade humana é a principal responsável pela poluição plástica nos oceanos."
+                ],
+                proxima: 2,
+            },
+            {
+                texto: "Você concorda com Clara? Explique por quê.",
+                afirmacao: [
+                    "Sim, Clara está certa. O lixo plástico nos oceanos é predominantemente causado pelo descarte inadequado de plásticos.",
+                    "A poluição plástica é um problema sério causado por ações humanas e não por fontes naturais."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Ana e Pedro estão debatendo sobre a origem do lixo plástico nos oceanos. Ana argumenta que a principal fonte de poluição plástica nos oceanos são os resíduos descartados incorretamente por atividades humanas, como o descarte inadequado de plásticos. Pedro discorda, sugerindo que a maior parte do plástico nos oceanos vem de fontes naturais.?",
+        enunciado: "Em sua jornada, Clara encontra Maria e João discutindo sobre as energias renováveis. Maria acredita que energias renováveis como solar e eólica são benéficas para o meio ambiente, enquanto João acha que essas fontes são caras e menos confiáveis comparadas às fontes tradicionais.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Você concorda com Maria? Explique por quê.",
                 afirmacao: [
-                    "Ana está certa. A maior parte do lixo plástico nos oceanos vem de atividades humanas, como descarte inadequado de plásticos e microplásticos..",
-                    "Pedro está enganado. A poluição plástica nos oceanos é predominantemente causada por ações humanas, não por fontes naturais.",
-                    
+                    "Sim, Maria está correta. As energias renováveis são mais limpas e têm se tornado mais competitivas em termos de custo.",
+                    "João está equivocado. As energias renováveis ajudam a reduzir as emissões de gases poluentes e têm um futuro promissor."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Carlos e Sofia discutem sobre o impacto das emissões de gases de efeito estufa no aquecimento global. Carlos argumenta que o aumento das emissões de gases como CO2 e metano contribui diretamente para o aquecimento do planeta. Sofia discorda, afirmando que as mudanças climáticas são causadas principalmente por variações naturais no clima.",
+                texto: "Você concorda com João? Explique por quê.",
                 afirmacao: [
-                    "Carlos está correto. O aumento das emissões de gases de efeito estufa está diretamente relacionado ao aquecimento global observado nas últimas décadas.",
-                    "Sofia está equivocada. As atividades humanas, especialmente a queima de combustíveis fósseis,",
-                    
+                    "Não, João está errado. Embora as energias renováveis possam ter um custo inicial mais alto, elas são mais sustentáveis e ajudam a proteger o meio ambiente.",
+                    "Maria está certa sobre as vantagens das energias renováveis."
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Clara lê sobre o impacto das mudanças climáticas na biodiversidade das florestas tropicais. Ela vê um debate entre dois especialistas: um acredita que as florestas tropicais são essenciais para a biodiversidade global, enquanto o outro acha que elas não têm um impacto significativo.",
+        alternativas: [
+            {
+                texto: "Você concorda com o especialista que defende a importância das florestas tropicais? Explique por quê.",
+                afirmacao: [
+                    "Sim, eu concordo. As florestas tropicais são extremamente importantes para a biodiversidade, abrigando uma grande variedade de espécies.",
+                    "O outro especialista está errado; as florestas tropicais têm um papel fundamental na biodiversidade global."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Você concorda com o especialista que minimiza a importância das florestas tropicais? Explique por quê.",
+                afirmacao: [
+                    "Não, o especialista está equivocado. As florestas tropicais são vitais para a biodiversidade e a saúde dos ecossistemas globais.",
+                    "Clara tem razão ao reconhecer a importância das florestas tropicais para a biodiversidade."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Lucas e Mariana discutem sobre os benefícios das energias renováveis. Lucas argumenta que as energias renováveis, como solar e eólica, são formas limpas de geração de energia que reduzem as emissões de gases poluentes. Mariana discorda, sugerindo que as energias renováveis são menos confiáveis e mais caras do que as fontes tradicionais de energia.?",
+        enunciado: "Clara visita um centro de pesquisa marinha e aprende sobre a acidificação dos oceanos. Ela lê que a acidificação é causada pela absorção de CO2 atmosférico, o que pode prejudicar os recifes de coral. Seu amigo Pedro acredita que a acidificação não representa uma grande ameaça para a vida marinha.",
         alternativas: [
             {
-                texto: "Lucas está certo. As energias renováveis são formas limpas e sustentáveis de geração de energia",
+                texto: "Você concorda com Pedro? Explique por quê.",
                 afirmacao: [
-                    "Mariana está enganada. As energias renováveis têm se tornado cada vez mais competitivas em termos de custo ",
-                    
-                ],
-                proxima: 3,
-            },
-            {
-                texto: "Joana e André debatem sobre a importância da conservação das florestas tropicais. Joana argumenta que as florestas tropicais são vitais para a biodiversidade global, abrigando milhões de espécies de plantas e animais. André discorda, sugerindo que as florestas tropicais não têm influência significativa na biodiversidade global.",
-                afirmacao: [
-                    "Joana está correta. As florestas tropicais são consideradas os ecossistemas mais biodiversos do planeta",
-                    "André está equivocado. As florestas tropicais são fundamentais para a biodiversidade global",
-                    
-                ],
-            },
-        ]
-    },
-    {
-        enunciado: "Daniel e Paula discutem sobre os efeitos da acidificação dos oceanos. Daniel argumenta que a acidificação dos oceanos está aumentando devido à absorção de CO2 atmosférico, o que ameaça os ecossistemas marinhos como recifes de coral. Paula discorda, afirmando que a acidificação dos oceanos não representa uma ameaça significativa para a vida marinha.",
-        alternativas: [
-            {
-            
-                afirmacao: [
-                    "Daniel está certo. A acidificação dos oceanos é uma preocupação crescente devido à absorção de CO2",
-                    "Paula está errada. A acidificação dos oceanos representa uma ameaça real para os ecossistemas marinhos"
+                    "Não, Pedro está errado. A acidificação dos oceanos é uma ameaça significativa para os ecossistemas marinhos, incluindo os recifes de coral.",
+                    "Clara está correta ao se preocupar com a acidificação dos oceanos."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Clara é uma jovem ambientalista que vive em uma pequena cidade cercada por belas florestas e um lago cristalino. Recentemente, Clara percebeu que as mudanças climáticas estão afetando a sua cidade. Decidida a ajudar, ela embarca em uma jornada para entender e combater esses problemas",
+                texto: "Você concorda com Clara? Explique por quê.",
                 afirmacao: [
-                    "Aumento da temperatura global devido à queima de combustíveis fósseis",
-                    "O plantio de novas árvores"
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "Clara sabia que a queima de combustíveis fósseis era um problema sério e escolheu a resposta correta.",
-        alternativas: [
-            {
-                texto: "Clara segue para o lago e encontra a água turva e os peixes lutando para respirar. Ela lê um aviso que diz:",
-                afirmacao: [
-                    "Oxigênio",
-                    "Dióxido de carbono (CO2)"
+                    "Sim, Clara está certa. A acidificação dos oceanos é um problema crescente que afeta negativamente a vida marinha.",
+                    "Pedro está equivocado ao minimizar os impactos da acidificação dos oceanos."
                 ],
                 proxima: 5,
             },
-            {
-                texto: "O que é o efeito estufa?",
-                afirmacao: [
-                    " Aquecimento da atmosfera devido a gases que retêm calor",
-                    "Resfriamento da atmosfera devido a gases que bloqueiam o sol"
-                ],
-                proxima: 6,
-            },
         ]
     },
     {
-        enunciado: "Clara visita a costa e vê que o nível do mar está subindo, ameaçando as casas dos moradores. Ela lê uma placa informativa:",
+        enunciado: "Clara percebe que a queima de combustíveis fósseis está contribuindo para o aumento da temperatura global. Ela se pergunta sobre as consequências desse fenômeno, como o aumento do nível do mar e os impactos em áreas costeiras.",
         alternativas: [
             {
                 texto: "Qual é a consequência comum das mudanças climáticas nas áreas costeiras?",
@@ -115,29 +113,21 @@ export const perguntas = [
                     "Aumento dos níveis do mar",
                     "Redução da temperatura global"
                 ],
-                proxima: 7,
+                proxima: 6,
             },
             {
-                texto: "O que é desmatamento e como ele contribui para as mudanças climáticas?",
+                texto: "O que é o efeito estufa?",
                 afirmacao: [
-                    "Remoção de árvores, reduzindo a absorção de CO2",
-                    "Plantio de novas árvores, aumentando a absorção de CO2"
+                    "Aquecimento da atmosfera devido a gases que retêm calor",
+                    "Resfriamento da atmosfera devido a gases que bloqueiam o sol"
                 ],
-                proxima: 7,
+                proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Clara encontra um centro de energia renovável em sua cidade e vê um display sobre diferentes fontes de energia:",
+        enunciado: "Clara visita um centro de energia renovável e vê um display sobre diferentes fontes de energia. Ela lê sobre a importância de fontes como a energia solar na redução das emissões de gases poluentes.",
         alternativas: [
-            {
-                texto: "O que é desmatamento e como ele contribui para as mudanças climáticas?",
-                afirmacao: [
-                    "Remoção de árvores, reduzindo a absorção de CO2",
-                    "Plantio de novas árvores, aumentando a absorção de CO2"
-                ],
-                proxima: 7,
-            },
             {
                 texto: "Qual é a principal fonte de energia renovável usada para reduzir a emissão de gases?",
                 afirmacao: [
@@ -146,10 +136,18 @@ export const perguntas = [
                 ],
                 proxima: 7,
             },
+            {
+                texto: "O que é desmatamento e como ele contribui para as mudanças climáticas?",
+                afirmacao: [
+                    "Remoção de árvores, reduzindo a absorção de CO2",
+                    "Plantio de novas árvores, aumentando a absorção de CO2"
+                ],
+                proxima: 7,
+            },
         ]
     },
     {
-        enunciado: "Clara visita um centro de pesquisa marinha e lê sobre os impactos das mudanças climáticas nos oceanos",
+        enunciado: "Clara visita um centro de pesquisa marinha e lê sobre os impactos das mudanças climáticas nos oceanos. Ela aprende que o aumento da temperatura global está afetando os ecossistemas marinhos de várias maneiras.",
         alternativas: [
             {
                 texto: "Como o aumento da temperatura global afeta os ecossistemas marinhos?",
@@ -159,11 +157,10 @@ export const perguntas = [
                 ],
             },
             {
-                texto: "Qual maior problema do aquecimeto global?",
+                texto: "Qual é o maior problema do aquecimento global?",
                 afirmacao: [
-                    "derretimento das calotas polares e o consequente aumento do nível dos oceanos",
-                    "ajuda os animais marinhos",
-    
+                    "Derretimento das calotas polares e o consequente aumento do nível dos oceanos",
+                    "Ajuda os animais marinhos"
                 ],
             },
         ]
