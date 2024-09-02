6159c79e1ad1e33b6608a23c6a962397ac35a066
Patozn <160489571+Patozn@users.noreply.github.com>
2024-09-02 08:57:26 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,168 +1,88 @@
 export const perguntas = [
     {
-        enunciado: "Clara, uma jovem ambientalista, está conversando com seu amigo Pedro sobre a importância das abelhas para o meio ambiente. Clara acredita que as abelhas são essenciais para a polinização e a produção de alimentos. Pedro acha que as abelhas não têm um impacto significativo na agricultura.",
+        enunciado: "Assim que você termina um projeto de escola sobre o meio ambiente, se depara com uma nova tecnologia que pode prever o impacto ambiental de diversas atividades. Qual o seu primeiro pensamento?",
         alternativas: [
             {
-                texto: "Você concorda com Clara? Explique por quê.",
+                texto: "Isso é preocupante!",
                 afirmacao: [
-                    "Sim, eu concordo com Clara. As abelhas são cruciais para a polinização de muitas plantas cultivadas, o que é vital para a produção de alimentos.",
-                    "Pedro está errado. Sem as abelhas, muitas culturas agrícolas não teriam uma boa produção."
+                    "Você se sentiu inquieto com a precisão e as possíveis consequências dessas previsões.",
+                    "Achou preocupante pensar na velocidade com que a tecnologia pode modificar nosso ambiente."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Você concorda com Pedro? Explique por quê.",
+                texto: "Isso é ótimo!",
                 afirmacao: [
-                    "Não, Pedro está equivocado. As abelhas desempenham um papel fundamental na agricultura e na produção de alimentos.",
-                    "Clara está correta ao destacar a importância das abelhas para o meio ambiente."
-                ],
-                proxima: 1,
-            },
-        ]
-    },
-    {
-        enunciado: "Clara visita uma praia e observa que há muito lixo plástico na água. Ela se lembra da discussão que teve com Pedro e decide investigar a origem do lixo. Clara descobre que muitos resíduos plásticos vêm do descarte inadequado por atividades humanas. Seu amigo Lucas acha que a poluição plástica nos oceanos tem uma origem natural.",
-        alternativas: [
-            {
-                texto: "Você concorda com Lucas? Explique por quê.",
-                afirmacao: [
-                    "Não, Lucas está incorreto. A maior parte da poluição plástica nos oceanos vem do descarte inadequado de plásticos e não de fontes naturais.",
-                    "A atividade humana é a principal responsável pela poluição plástica nos oceanos."
-                ],
-                proxima: 2,
-            },
-            {
-                texto: "Você concorda com Clara? Explique por quê.",
-                afirmacao: [
-                    "Sim, Clara está certa. O lixo plástico nos oceanos é predominantemente causado pelo descarte inadequado de plásticos.",
-                    "A poluição plástica é um problema sério causado por ações humanas e não por fontes naturais."
+                    "Quis explorar como essa tecnologia pode ajudar na preservação do meio ambiente.",
+                    "Pensou que a tecnologia pode ser uma ferramenta poderosa para promover a sustentabilidade."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Em sua jornada, Clara encontra Maria e João discutindo sobre as energias renováveis. Maria acredita que energias renováveis como solar e eólica são benéficas para o meio ambiente, enquanto João acha que essas fontes são caras e menos confiáveis comparadas às fontes tradicionais.",
+        enunciado: "Considerando a importância da preservação ambiental, a professora pediu um trabalho sobre o impacto da tecnologia na conservação dos recursos naturais. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Você concorda com Maria? Explique por quê.",
+                texto: "Utiliza uma ferramenta de pesquisa que analisa dados ambientais para encontrar informações relevantes para o trabalho e explica de maneira clara.",
                 afirmacao: [
-                    "Sim, Maria está correta. As energias renováveis são mais limpas e têm se tornado mais competitivas em termos de custo.",
-                    "João está equivocado. As energias renováveis ajudam a reduzir as emissões de gases poluentes e têm um futuro promissor."
+                    "Você conseguiu utilizar a tecnologia para encontrar informações valiosas.",
+                    "Percebeu que a tecnologia pode facilitar a compreensão de dados complexos sobre o meio ambiente.",
+                    "Achou útil como a tecnologia pode simplificar informações técnicas para promover a conservação."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Você concorda com João? Explique por quê.",
+                texto: "Escreve o trabalho com base nas conversas com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    "Não, João está errado. Embora as energias renováveis possam ter um custo inicial mais alto, elas são mais sustentáveis e ajudam a proteger o meio ambiente.",
-                    "Maria está certa sobre as vantagens das energias renováveis."
-                ],
-                proxima: 3,
-            },
-        ]
-    },
-    {
-        enunciado: "Clara lê sobre o impacto das mudanças climáticas na biodiversidade das florestas tropicais. Ela vê um debate entre dois especialistas: um acredita que as florestas tropicais são essenciais para a biodiversidade global, enquanto o outro acha que elas não têm um impacto significativo.",
-        alternativas: [
-            {
-                texto: "Você concorda com o especialista que defende a importância das florestas tropicais? Explique por quê.",
-                afirmacao: [
-                    "Sim, eu concordo. As florestas tropicais são extremamente importantes para a biodiversidade, abrigando uma grande variedade de espécies.",
-                    "O outro especialista está errado; as florestas tropicais têm um papel fundamental na biodiversidade global."
-                ],
-                proxima: 4,
-            },
-            {
-                texto: "Você concorda com o especialista que minimiza a importância das florestas tropicais? Explique por quê.",
-                afirmacao: [
-                    "Não, o especialista está equivocado. As florestas tropicais são vitais para a biodiversidade e a saúde dos ecossistemas globais.",
-                    "Clara tem razão ao reconhecer a importância das florestas tropicais para a biodiversidade."
+                    "Sentiu que utilizar suas próprias fontes e métodos é mais pessoal e direto.",
+                    "Preferiu utilizar métodos tradicionais, mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de receio sobre como a tecnologia pode manipular dados e preferiu uma abordagem mais convencional."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Clara visita um centro de pesquisa marinha e aprende sobre a acidificação dos oceanos. Ela lê que a acidificação é causada pela absorção de CO2 atmosférico, o que pode prejudicar os recifes de coral. Seu amigo Pedro acredita que a acidificação não representa uma grande ameaça para a vida marinha.",
+        enunciado: "Você precisa participar de um debate sobre como a tecnologia pode ajudar a mitigar os efeitos das mudanças climáticas. Qual posicionamento você toma?",
         alternativas: [
             {
-                texto: "Você concorda com Pedro? Explique por quê.",
+                texto: "Defende a ideia de que a tecnologia pode oferecer novas soluções para a proteção ambiental e melhorar as práticas sustentáveis.",
                 afirmacao: [
-                    "Não, Pedro está errado. A acidificação dos oceanos é uma ameaça significativa para os ecossistemas marinhos, incluindo os recifes de coral.",
-                    "Clara está correta ao se preocupar com a acidificação dos oceanos."
+                    "Acredita que a tecnologia pode criar novas oportunidades para a preservação ambiental.",
+                    "Participa ativamente no desenvolvimento de soluções inovadoras para a proteção do meio ambiente."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Você concorda com Clara? Explique por quê.",
+                texto: "Preocupa-se com a possibilidade de que a tecnologia possa causar novos problemas ambientais e defende a importância de uma abordagem equilibrada.",
                 afirmacao: [
-                    "Sim, Clara está certa. A acidificação dos oceanos é um problema crescente que afeta negativamente a vida marinha.",
-                    "Pedro está equivocado ao minimizar os impactos da acidificação dos oceanos."
-                ],
-                proxima: 5,
-            },
-        ]
-    },
-    {
-        enunciado: "Clara percebe que a queima de combustíveis fósseis está contribuindo para o aumento da temperatura global. Ela se pergunta sobre as consequências desse fenômeno, como o aumento do nível do mar e os impactos em áreas costeiras.",
-        alternativas: [
-            {
-                texto: "Qual é a consequência comum das mudanças climáticas nas áreas costeiras?",
-                afirmacao: [
-                    "Aumento dos níveis do mar",
-                    "Redução da temperatura global"
-                ],
-                proxima: 6,
-            },
-            {
-                texto: "O que é o efeito estufa?",
-                afirmacao: [
-                    "Aquecimento da atmosfera devido a gases que retêm calor",
-                    "Resfriamento da atmosfera devido a gases que bloqueiam o sol"
+                    "Sua preocupação levou você a formar um grupo para discutir o uso responsável da tecnologia na conservação.",
+                    "Criou iniciativas voltadas para a ética no uso de tecnologia ambiental para evitar impactos negativos."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Clara visita um centro de energia renovável e vê um display sobre diferentes fontes de energia. Ela lê sobre a importância de fontes como a energia solar na redução das emissões de gases poluentes.",
+        enunciado: "Após a discussão, você precisa criar uma imagem que represente o papel da tecnologia na proteção ambiental. O que você faz?",
         alternativas: [
             {
-                texto: "Qual é a principal fonte de energia renovável usada para reduzir a emissão de gases?",
+                texto: "Cria uma imagem usando uma ferramenta de design tradicional.",
                 afirmacao: [
-                    "Energia solar",
-                    "Energia de carvão"
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "O que é desmatamento e como ele contribui para as mudanças climáticas?",
-                afirmacao: [
-                    "Remoção de árvores, reduzindo a absorção de CO2",
-                    "Plantio de novas árvores, aumentando a absorção de CO2"
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "Clara visita um centro de pesquisa marinha e lê sobre os impactos das mudanças climáticas nos oceanos. Ela aprende que o aumento da temperatura global está afetando os ecossistemas marinhos de várias maneiras.",
-        alternativas: [
-            {
-                texto: "Como o aumento da temperatura global afeta os ecossistemas marinhos?",
-                afirmacao: [
-                    "Acidificação dos oceanos e branqueamento de corais",
-                    "Diminuição da acidez dos oceanos"
+                    "Você percebeu que muitas pessoas ainda não conhecem ferramentas tradicionais e decidiu compartilhar suas habilidades.",
+                    "Ainda acredita que métodos tradicionais são melhores para expressar a criatividade e incentiva outros a usar técnicas manuais."
                 ],
             },
             {
-                texto: "Qual é o maior problema do aquecimento global?",
+                texto: "Cria uma imagem usando um gerador de imagens com tecnologia de IA.",
                 afirmacao: [
-                    "Derretimento das calotas polares e o consequente aumento do nível dos oceanos",
-                    "Ajuda os animais marinhos"
+                    "Você acelerou o processo de criação e agora ensina outras pessoas a usar ferramentas de IA para expressar ideias ambientais.",
+                    "Compartilhou suas criações para ajudar a comunicar a importância da tecnologia na proteção do meio ambiente.",
+                    "Viu que muitas pessoas têm dificuldade em criar imagens e acredita que a IA pode ajudar a tornar essas ideias visíveis."
                 ],
             },
         ]
     }
 ];
+
